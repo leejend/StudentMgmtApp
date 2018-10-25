3563f632b896e4a2ed37f5abb0a30767265bd261
leejend <jeanettelee09@optusnet.com.au>
2018-10-25 11:05:52 +1100

    Initial Commit

---
+++ StudentMgmtApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentMgmtApp
+//
+//  Created by Jeanette Lee on 10/25/18.
+//  Copyright © 2018 Jeanette Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
