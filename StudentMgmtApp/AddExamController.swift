8ea176e72f8f4a35c14e960369a890789cd559f1
leejend <jeanettelee09@optusnet.com.au>
2018-10-31 23:05:48 +1100

    user profile

--- StudentMgmtApp/AddExamController.swift
+++ StudentMgmtApp/AddExamController.swift
@@ -17,16 +17,37 @@ class AddExamController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 }
-    
+
    
     
    
     @IBAction func addExam(_ sender: Any) {
+        //Referencing date
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
         let strDate = dateFormatter.string(from: datePicker.date)
         dateTime.text = strDate
 
+        //Ref date
+//        let dateFormatter = NSDateFormatter()
+//        dateFormatter.dateFormat = "MM/dd/yyyy"
+//        let someDate = dateFormatter.dateFromString("03/10/2015")
+        
+//        //Get calendar
+//        let calendar = NSCalendar.currentCalendar()
+//        
+//        //Get just MM/dd/yyyy from current date
+//        let flags = NSCalendar.Unit.CalendarUnitDay | NSCalendar.Unit.CalendarUnitMonth | NSCalendar.Unit.CalendarUnitYear
+//        let components = calendar.components(flags, fromDate: NSDate())
+//        
+//        //Convert to NSDate
+//        let today = calendar.dateFromComponents(components)
+//        
+//        if strDate!.timeIntervalSinceDate(today!).isSignMinus {
+//            //someDate is berofe than today
+//        } else {
+//            //someDate is equal or after than today
+//        }
     
         // get the AppDelegate object
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
---
+++ StudentMgmtApp/userProfileController.swift
@@ -0,0 +1,25 @@
+//
+//  userProfileController.swift
+//  StudentMgmtApp
+//
+//  Created by Jeanette Lee on 10/31/18.
+//  Copyright © 2018 Jeanette Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class userProfileController: UIViewController {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       }
+    
+    
+    
+}
